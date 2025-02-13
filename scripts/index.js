79b66a51fbc42d94ffb0e2e90f645e5bb280b1be
Ilya Markovkin work <markovidzeiloy@gmail.com>
2025-02-13 17:36:15 +0300

    Первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
-// @todo: Темплейт карточки
+const CardTemplate = document.querySelector('#card-template');
+const container = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function CreateCard (region) {
+  const card = CardTemplate.content.cloneNode(true).querySelector(".card");
+  const deletecard = card.querySelector('.card__delete-button');
+  card.querySelector('.card__image').setAttribute('src', region.link);
+  card.querySelector('.card__title').textContent = region.name;
+  container.prepend(card);
+  deletecard.addEventListener('click', function() {
+    card.remove();
+    });
+};
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(CreateCard);
