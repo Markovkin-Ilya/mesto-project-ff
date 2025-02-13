91b1cc3a6eb6db7ba567e070e9798d13c322ec34
Ilya Markovkin <Skopigand@yandex.ru>
2025-02-14 01:51:43 +0300

    Написал отдельную функцию удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,20 @@
-const CardTemplate = document.querySelector('#card-template');
+const cardTemplate = document.querySelector('#card-template');
 const container = document.querySelector(".places__list");
 
-function CreateCard (region) {
-  const card = CardTemplate.content.cloneNode(true).querySelector(".card");
-  const deletecard = card.querySelector('.card__delete-button');
-  card.querySelector('.card__image').setAttribute('src', region.link);
-  card.querySelector('.card__title').textContent = region.name;
-  container.prepend(card);
-  deletecard.addEventListener('click', function() {
-    card.remove();
-    });
+function createCard (region, deleteCallback) {
+  const cardElement = cardTemplate.content.cloneNode(true).querySelector(".card");
+  const deleteElement = cardElement.querySelector('.card__delete-button');
+  const foto = cardElement.querySelector('.card__image');
+  foto.setAttribute('src', region.link);
+  foto.setAttribute('alt', region.name);
+  cardElement.querySelector('.card__title').textContent = region.name;
+  deleteElement.addEventListener('click',  () => deleteCallback(cardElement));
+  return cardElement;
 };
 
-initialCards.forEach(CreateCard);
+function deleteCard(item){
+  item.remove()} 
+
+initialCards.forEach(item => {
+  container.append(createCard(item, deleteCard));
+});
